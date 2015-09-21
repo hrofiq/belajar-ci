8f98c6cf13eecb603c0f1551eea1fe923a4eab16
Husni Rofiq <husnirofiq@sandiloka.com>
2015-09-21 11:05:42 +0700

    Init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+   add: function(a, b) {
+	return a + b;
+   },
+   kurang: function(a, b) {
+	return a - b;
+   }
+
+}
---
+++ test.js
@@ -0,0 +1,18 @@
+var op = require('./')
+
+describe ('Hello module', function() {
+
+    // set of test
+    it ('should return result of accumulation two numbers', function() {
+	var result = op.add(1, 2);
+	result.should.equal(3);
+
+    });
+
+    it ('should return result of accumulation two numbers', function() {
+        var result = op.kurang(1, 2);
+        result.should.equal(-1);
+
+    });
+
+});
